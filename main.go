89970f13ea7f5b0afa12a4cbc97c7ca51342ab3c
Adler <nkj20932@hotmail.com>
2017-12-18 21:52:07 +1100

    allow draw a list and movement

--- main.go
+++ main.go
@@ -4,39 +4,71 @@ import (
 	// "fmt"
 	"os"
 
-	"github.com/nsf/termbox-go"
+	tb "github.com/nsf/termbox-go"
 )
 
+var dc tb.Attribute = tb.ColorDefault
+var list []string = []string{"localhost", "staging", "production"}
+var listIndex int
+
 func main() {
-	err := termbox.Init()
+	err := tb.Init()
 	if err != nil {
 		panic(err)
 	}
-	defer termbox.Close()
+	defer tb.Close()
 
-	termbox.SetInputMode(termbox.InputEsc)
-	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	drawList()
 
-	tbprint(0, 0, "localhost")
-	tbprint(0, 1, "staging")
-	tbprint(0, 2, "production")
-
-	termbox.Flush()
+	tb.SetInputMode(tb.InputEsc)
+	tb.Clear(dc, dc)
 
 	for {
-		switch event := termbox.PollEvent(); event.Type {
-		case termbox.EventKey:
+		switch event := tb.PollEvent(); event.Type {
+		case tb.EventKey:
 			if isExit(event) {
 				os.Exit(0)
 			}
-		case termbox.EventError:
+			switch event.Ch {
+			// j
+			case 106:
+				listIndex += 1
+			// k
+			case 107:
+				listIndex -= 1
+			}
+			drawList()
+		case tb.EventError:
 			panic(event.Err)
 		}
 	}
 }
 
-func isExit(event termbox.Event) bool {
-	if event.Key == termbox.KeyEsc ||
+func drawList() {
+	var listXOffset int = 0
+	var listYOffset int = 2
+
+	tbprint(listXOffset, 0, "Databases", dc, dc)
+	tbprint(listXOffset, 1, "---------", dc, dc)
+
+	for i := 0; i < len(list); i++ {
+		if listIndex < 0 {
+			listIndex = 0
+		}
+		if listIndex > len(list)-1 {
+			listIndex = len(list) - 1
+		}
+		if i == listIndex {
+			tbprint(listXOffset, i+listYOffset, list[i], dc, tb.ColorGreen)
+		} else {
+			tbprint(listXOffset, i+listYOffset, list[i], dc, dc)
+		}
+	}
+	tb.Flush()
+}
+
+func isExit(event tb.Event) bool {
+	if event.Key == tb.KeyEsc ||
 		// q & Q
 		event.Ch == 113 || event.Ch == 81 {
 		return true
@@ -44,9 +76,9 @@ func isExit(event termbox.Event) bool {
 	return false
 }
 
-func tbprint(x int, y int, msg string) {
+func tbprint(x int, y int, msg string, fg tb.Attribute, bg tb.Attribute) {
 	for _, c := range msg {
-		termbox.SetCell(x, y, c, termbox.ColorDefault, termbox.ColorDefault)
+		tb.SetCell(x, y, c, fg, bg)
 		x += 1
 	}
 }
